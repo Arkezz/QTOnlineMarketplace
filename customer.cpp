a1a84eeab232fe9402274b7045865925cb1f89d0
= <40185507+Arkezz@users.noreply.github.com>
2022-11-01 10:32:03 +0200

    commit

--- customer.cpp
+++ customer.cpp
@@ -5,107 +5,148 @@
 #include "emptycart.h"
 #include "noavailablequantity.h"
 
-QVector<Product*> products;
-QVector<Product*> cart;
+QMap<QString, Product*> products;
+QMap<QString, Product*> cart;
 
-Customer::Customer(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::Customer)
+Customer::Customer(QWidget* parent) :
+	QWidget(parent),
+	ui(new Ui::Customer)
 {
-    ui->setupUi(this);
+	ui->setupUi(this);
 }
 
 Customer::~Customer()
 {
-    delete ui;
+	delete ui;
 }
 
-void Customer::addProduct(Product* a){
-    //Checks if the product already exists and if so only increases the quantity
-    for (int i = 0; i < products.size(); i++) {
-        if(products[i]->getName() == a->getName()){
-            products[i]->setQuantity(products[i]->getQuantity() + a->getQuantity());
-            return;
-        }
-    }
-    products.append(a);
+void Customer::addProduct(Product* a) {
+	//Checks if the product already exists and if so only increases the quantity
+	if (products.contains(a->getName())) {
+		products[a->getName()]->setQuantity(products[a->getName()]->getQuantity() + a->getQuantity());
+	}
+	else {
+		products[a->getName()] = a;
+	}
 }
 
 void Customer::setCustomerInformation(QString name, QString email, QString address, QString number)
 {
-    this->name = name;
-    this->email = email;
-    this->address = address;
-    this->number = number;
+	this->name = name;
+	this->email = email;
+	this->address = address;
+	this->number = number;
 }
 
 void Customer::on_searchProductButton_clicked()
 {
+	QString nameSearch = ui->searchNameText->text().toLower();
+	QString categorySearch = ui->searchCategoryText->text().toLower();
+	QListWidget* list = ui->productListText;
+	list->clear();
 
+	//Search for the product by name, category, or both
+	if (nameSearch != "" && categorySearch != "") {
+		for (auto i = products.begin(); i != products.end(); i++) {
+			if (i.value()->getName().toLower().contains(nameSearch) && i.value()->getCategory().toLower().contains(categorySearch)) {
+				//Check if the product is already in the list
+				if (!list->findItems(i.value()->getName(), Qt::MatchExactly).size()) {
+					list->addItem(i.value()->getName());
+				}
+			}
+		}
+	}
+	else if (nameSearch != "") {
+		for (auto i = products.begin(); i != products.end(); i++) {
+			if (i.value()->getName().toLower().contains(nameSearch)) {
+				if (!list->findItems(i.value()->getName(), Qt::MatchExactly).size()) {
+					list->addItem(i.value()->getName());
+				}
+			}
+		}
+	}
+	else if (categorySearch != "") {
+		for (auto i = products.begin(); i != products.end(); i++) {
+			if (i.value()->getCategory().toLower().contains(categorySearch)) {
+				if (!list->findItems(i.value()->getName(), Qt::MatchExactly).size()) {
+					list->addItem(i.value()->getName());
+				}
+			}
+		}
+	} //Else if nothing is found send a qmessage box
+	else {
+		QMessageBox::information(this, "Error", "No products found");
+	}
 }
 
 void Customer::on_addToCartButton_clicked()
 {
-    QMessageBox err;
-    QString total = "";
-    try {
-        //Always take strings in lower case to remove case sensetive situations
-        QString addProduct = ui->addProductText->text().toLower();
-        //Absolute value so negative numbers dont break it
-        int addQuantity = abs(ui->addQuantityText->text().toInt());
+	//Always take strings in lower case to remove case sensetive situations
+	QString addProduct = ui->addProductText->text().toLower();
+	//Absolute value so negative numbers dont break it
+	int addQuantity = abs(ui->addQuantityText->text().toInt());
+	//Cart list
+	QListWidget* list = ui->cartListText;
 
-        for (int i = 0; i < products.size(); i++) {
-            if(addProduct == products[i]->getName()){
-                if(products[i]->getQuantity() < addQuantity && addQuantity != 0){
-                    throw NoAvailableQuantity();
-                } else {
-                    for (int i = 0; i < cart.size(); i++) {
-                        if(cart[i]->getName() == addProduct){
-                            cart[i]->setQuantity(cart[i]->getQuantity() + addQuantity);
-                            products[i]->setQuantity(products[i]->getQuantity() - addQuantity);
-                            return;
-                        }
-                    }
-                    cart.append(new Product(products[i]));
-                    cart[cart.size() - 1]->setQuantity(addQuantity);
-                    products[i]->setQuantity(products[i]->getQuantity() - addQuantity);
-                    ui->cartListText->addItem(cart[i]->getName());
-                }
-            }
-        }
-    } catch (NoAvailableQuantity& e){
-        err.setText(e.what());
-        err.exec();
-    }
+	try {
+		//Display the product added to the cart on the cart list
+		if (products.contains(addProduct)) {
+			if (products[addProduct]->getQuantity() >= addQuantity && addQuantity != 0) {
+				//If the product is already in the cart, increase the quantity
+				if (cart.contains(addProduct)) {
+					cart[addProduct]->setQuantity(cart[addProduct]->getQuantity() + addQuantity);
+				}
+				else {
+					//If the product is not in the cart, add it
+					cart[addProduct] = products[addProduct];
+					cart[addProduct]->setQuantity(addQuantity);
+				}
+				//Display the product added to the cart on the cart list. Do not add it again if it is already in the list
+				if (!list->findItems(cart[addProduct]->getName(), Qt::MatchExactly).size()) {
+					list->addItem(cart[addProduct]->getName());
+				}
+				//Decrease the quantity of the product in the store
+				products[addProduct]->setQuantity(products[addProduct]->getQuantity() - addQuantity);
+			}
+			else {
+				throw NoAvailableQuantity();
+			}
+		}
+		else {
+			throw NoAvailableQuantity();
+		}
+	}
+	catch (NoAvailableQuantity& e) {
+		QMessageBox::information(this, "Error", e.what());
+	}
 }
 
 void Customer::on_confirmBuyingButton_clicked()
 {
-    QMessageBox msg, err;
-    int total = 0;
-    QString receipt = "Product:\tPrice:\tQuantity:\n";
-    try {
-        if(cart.empty()){
-            throw EmptyCart();
-        } else {
-            for (int i = 0; i < cart.size(); i++) {
-                receipt += cart[i]->getName() + "\t" + QString::number(cart[i]->getPrice()) + "\t" + QString::number(cart[i]->getQuantity()) + "\n";
-                total+= cart[i]->getPrice() * cart[i]->getQuantity();
-            }
-            msg.setText(receipt + "Total Price: " + QString::number(total));
-            msg.exec();
-        }
-    } catch(EmptyCart& e){
-        err.setText(e.what());
-        err.exec();
-    }
+	//Show the user a receipt of all his products in the cart and the total price
+	QString receipt = "Receipt for " + name + ":\n";
+	double totalPrice = 0;
+	try {
+		if (cart.size() == 0) {
+			throw EmptyCart();
+		}
+		for (auto i = cart.begin(); i != cart.end(); i++) {
+			//Show the receipt in a product quantity price form
+			receipt += i.value()->getName() + " " + QString::number(i.value()->getQuantity()) + " " + QString::number(i.value()->getPrice()) + "\n";
+			totalPrice += i.value()->getPrice() * i.value()->getQuantity();
+		}
+		receipt += "Total Price: $" + QString::number(totalPrice);
+		QMessageBox::information(this, "Receipt", receipt);
+	}
+	catch (EmptyCart e) {
+		QMessageBox::information(this, "Error", e.what());
+	}
 }
 
 void Customer::on_customerBackButton_clicked()
 {
-    regWindow = new RegisterationWindow();
-    regWindow->setWindowTitle("Customer Registeration");
-    regWindow->show();
-    this->close();
+	regWindow = new RegisterationWindow();
+	regWindow->setWindowTitle("Customer Registeration");
+	regWindow->show();
+	this->close();
 }
-
--- customer.h
+++ customer.h
@@ -2,43 +2,45 @@
 #define CUSTOMER_H
 
 #include <QWidget>
-#include <vector>
 #include <QMessageBox>
+#include <QMap>
 
 namespace Ui {
-class Customer;
+	class Customer;
 }
 
 class RegisterationWindow;
 class Product;
 
-class Customer :  public QWidget
+class Customer : public QWidget
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    explicit Customer(QWidget *parent = nullptr);
-    void setCustomerInformation(QString name, QString email, QString address, QString number);
-    ~Customer();
+	explicit Customer(QWidget* parent = nullptr);
+	void setCustomerInformation(QString name, QString email, QString address, QString number);
+	~Customer();
 
-    void addProduct(Product*);
+	void addProduct(Product*);
 
 private slots:
-    void on_searchProductButton_clicked();
+	void on_searchProductButton_clicked();
 
-    void on_customerBackButton_clicked();
+	void on_customerBackButton_clicked();
 
-    void on_addToCartButton_clicked();
+	void on_addToCartButton_clicked();
 
-    void on_confirmBuyingButton_clicked();
+	void on_confirmBuyingButton_clicked();
 
 private:
-    Ui::Customer *ui;
-    RegisterationWindow* regWindow;
-    QString name;
-    QString email;
-    QString address;
-    QString number;
+	Ui::Customer* ui;
+	RegisterationWindow* regWindow;
+	QString name;
+	QString email;
+	QString address;
+	QString number;
+	QMap<QString, Product*> products;
+	QMap<QString, Product*> cart;
 };
 
 #endif // CUSTOMER_H
